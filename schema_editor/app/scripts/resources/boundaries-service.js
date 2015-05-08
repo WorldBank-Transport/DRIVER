844a57e124a31b6db39ea72ac27f83e5859951c4
Nathan Zimmerman <npzimmerman@gmail.com>
2015-05-08 16:47:17 -0400

    Add transformation to remove extraneous data from query

--- schema_editor/app/scripts/resources/boundaries-service.js
+++ schema_editor/app/scripts/resources/boundaries-service.js
@@ -3,9 +3,25 @@
 
     /* ngInject */
     function Boundaries($resource, Config) {
-      var urlString = Config.api.hostname + '/api/boundaries/:id';
+        var urlString = Config.api.hostname + '/api/boundaries/:id';
 
-      return $resource(urlString, {id: '@uuid'});
+        var resource = {
+          'query': stdResource.query,
+          'get': stdResource.get,
+          'post': postWithFile,
+          'remove': stdResource.remove,
+          'delete': stdResource.delete
+        };
+
+        var stdResource = $resource(urlString, {id: '@uuid'}, {
+            query: {
+                method: 'GET',
+                transformResponse: function(data) { return angular.fromJson(data).results; },
+                isArray: true
+            }
+        });
+
+        return resource;
     }
 
     angular.module('ase.resources')
