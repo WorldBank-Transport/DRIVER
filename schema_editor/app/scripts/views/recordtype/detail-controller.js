91661867096e43f403fdc083ee74dcf59e9fefd4
CloudNiner <andrew.fink.11@gmail.com>
2015-05-12 17:53:04 -0400

    Delete related content types

--- schema_editor/app/scripts/views/recordtype/detail-controller.js
+++ schema_editor/app/scripts/views/recordtype/detail-controller.js
@@ -4,6 +4,7 @@
     /* ngInject */
     function RTDetailController($stateParams, RecordSchemas, RecordTypes) {
         var ctl = this;
+        ctl.deleteSchema = deleteSchema;
         initialize();
 
         function initialize() {
@@ -11,6 +12,17 @@
                 ctl.recordType = data;
                 ctl.currentSchema = RecordSchemas.get({ id: ctl.recordType.current_schema });
             });
+        }
+
+        function deleteSchema(key) {
+            if (ctl.currentSchema.schema.definitions[key]) {
+                delete ctl.currentSchema.schema.definitions[key];
+                // TODO: Error handle and revert delete if failure
+                RecordSchemas.create({
+                    record_type: ctl.recordType.uuid,
+                    schema: ctl.currentSchema.schema
+                });
+            }
         }
     }
 
