c2b56df3976a4ea96ecfa78a98d7dc1f05882662
CloudNiner <andrew.fink.11@gmail.com>
2015-05-13 11:39:41 -0400

    Add RTListController.initialize test

---
+++ schema_editor/test/spec/views/recordtype/list-controller.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+describe('ase.views.recordtype: ListController', function () {
+
+    beforeEach(module('ase.views.recordtype'));
+    beforeEach(module('ase.mock.resources'));
+
+    var $controller;
+    var $httpBackend;
+    var $rootScope;
+    var $scope;
+    var RecordTypes;
+    var Controller;
+    var ResourcesMock;
+    //var $stateParams = {};
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function (_$controller_, _$httpBackend_, _$rootScope_,
+                                _ResourcesMock_, _RecordTypes_) {
+        $controller = _$controller_;
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        ResourcesMock = _ResourcesMock_;
+        RecordTypes = _RecordTypes_;
+    }));
+
+    it('should make an httprequest for active recordtypes on controller initialize', function () {
+        var requestUrl = /\/api\/recordtypes\/\?active=True/;
+        $httpBackend.expectGET(requestUrl).respond(200, ResourcesMock.RecordTypesResponse);
+
+        Controller = $controller('RTListController', {
+            $scope: $scope
+        });
+        $scope.$apply();
+
+        $httpBackend.flush();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+});
