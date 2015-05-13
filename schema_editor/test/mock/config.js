18d69e026cda70f7588b228e4c541946a57893e3
Kenny Shepard <kshepard@gmail.com>
2015-05-13 11:57:56 -0400

    Add config mock

---
+++ schema_editor/test/mock/config.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    var config = {
+        debug: true,
+        html5Mode: {
+            enabled: false,
+            prefix: '!'
+        },
+        api: {
+            hostname: 'http://localhost:7000'
+        }
+    };
+
+    angular.module('ase.config', [])
+    .constant('Config', config);
+})();
