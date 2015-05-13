3171e838fe535422b7e1e2a04eb8915409121030
CloudNiner <andrew.fink.11@gmail.com>
2015-05-13 13:11:02 -0400

    Add cancel-bubble directive to stop click events

---
+++ schema_editor/app/scripts/views/sidebar/cancel-bubble-directive.js
@@ -0,0 +1,31 @@
+
+(function () {
+    'use strict';
+
+    /* ngInject */
+    function CancelBubble($log) {
+        var module = {
+            restrict: 'A',
+            link: link
+        };
+        return module;
+
+        function link(scope, element) {
+
+            element.click(function (event) {
+                // All the cool kids
+                if (event.stopPropagation) {
+                    event.stopPropagation();
+                // IE8 & below
+                } else {
+                    $log.debug('Used cancelBubble in cancel-bubble directive');
+                    event.cancelBubble = true;
+                }
+            });
+        }
+    }
+
+    angular.module('ase.views.sidebar')
+    .directive('cancelBubble', CancelBubble);
+
+})();
