34744a20de747ec63937266ef9b6c58a58364117
Derek Dohler <ddohler@azavea.com>
2015-05-27 15:23:24 -0400

    Add serialization tests

--- schema_editor/test/spec/schemas/schemas-service.spec.js
+++ schema_editor/test/spec/schemas/schemas-service.spec.js
@@ -27,4 +27,61 @@ describe('ase.schemas:Schemas', function () {
         var field = Schemas.Fields.SelectList();
         expect(schemaEnv.validate('v4', field)).toBe(null);
     });
+
+    it('should add the $schema declaration to schemas', function () {
+        var obj = Schemas.JsonObject();
+        obj = Schemas.addVersion4Declaration(obj);
+        expect(schemaEnv.validate('v4', obj)).toBe(null);
+    });
+
+    it('should serialize schema form outputs to data form schema snippets', function () {
+        var schemaFormData = [{
+            fieldType: 'text',
+            isRequired: false,
+            fieldTitle: 'Text'
+        }];
+        var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
+        expect(dataFormSchemaDef).toEqual(jasmine.objectContaining({
+            required: jasmine.anything(),
+            properties: jasmine.anything(),
+            type: 'object'
+        }));
+    });
+
+    it('should include an enum key in selectlist fields', function () {
+        var schemaFormData = [{
+            fieldType: 'selectlist',
+            isRequired: false,
+            fieldTitle: 'Text',
+            fieldOptions: ['opt1', 'opt2', 'opt3']
+        }];
+        var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
+        expect(dataFormSchemaDef.properties.Text['enum']).toEqual(schemaFormData[0].fieldOptions);
+    });
+
+    it('should include a media key in image fields', function () {
+        var schemaFormData = [{
+            fieldType: 'image',
+            isRequired: false,
+            fieldTitle: 'Photo'
+        }];
+        var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
+        expect(dataFormSchemaDef.properties.Photo.media).toEqual(jasmine.anything());
+    });
+
+    it('should set the "required" key when fields have isRequired: true', function () {
+        var schemaFormData = [{
+            fieldType: 'selectlist',
+            isRequired: false,
+            fieldTitle: 'Text',
+            fieldOptions: ['opt1', 'opt2', 'opt3']
+        },{
+            fieldType: 'image',
+            isRequired: true,
+            fieldTitle: 'Photo'
+        }];
+        var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
+        expect(dataFormSchemaDef.required.length).toEqual(1);
+        expect(dataFormSchemaDef.required[0]).toEqual('Photo');
+    });
 });
