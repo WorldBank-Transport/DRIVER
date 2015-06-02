c163702de84e2b57c35dc2ed33adb46fc7279b99
Derek Dohler <ddohler@azavea.com>
2015-06-02 15:51:22 -0400

    Deserialize related type schemas to form

--- schema_editor/test/spec/schemas/schemas-service.spec.js
+++ schema_editor/test/spec/schemas/schemas-service.spec.js
@@ -72,6 +72,18 @@ describe('ase.schemas:Schemas', function () {
         expect(dataFormSchemaDef.properties.Text['enum']).toEqual(schemaFormData[0].fieldOptions);
     });
 
+    it('should serialize a displayType key in selectlist fields', function () {
+        var schemaFormData = [{
+            fieldType: 'selectlist',
+            displayType: 'select',
+            isRequired: false,
+            fieldTitle: 'Text',
+            fieldOptions: ['opt1', 'opt2', 'opt3']
+        }];
+        var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
+        expect(dataFormSchemaDef.properties.Text.displayType).toEqual(schemaFormData[0].displayType);
+    });
+
     it('should serialize a media key in image fields', function () {
         var schemaFormData = [{
             fieldType: 'image',
@@ -98,7 +110,7 @@ describe('ase.schemas:Schemas', function () {
         expect(dataFormSchemaDef.required[0]).toEqual('Photo');
     });
 
-    it('should net set the "required" key when there are no required fields', function () {
+    it('should not set the "required" key when there are no required fields', function () {
         var schemaFormData = [{
             fieldType: 'selectlist',
             isRequired: false,
@@ -111,6 +123,22 @@ describe('ase.schemas:Schemas', function () {
         }];
         var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
         expect(dataFormSchemaDef.required).toBeUndefined();
+    });
+
+    it('should calculate and set propertyOrder based on the ordering of fields', function () {
+        var schemaFormData = [];
+        for (var i in [0,1,2,3,4]) {
+            schemaFormData.push({
+                fieldType: 'text',
+                fieldTitle: 'Title' + i,
+                indexShouldBe: i
+            });
+        }
+        var dataFormSchemaDef = Schemas.definitionFromSchemaFormData(schemaFormData);
+        for (var j in [0,1,2,3,4]) {
+            var field = dataFormSchemaDef.properties['Title' + j];
+            expect(field.propertyOrder.toString()).toEqual(j);
+        }
     });
 
     it('should validate that no two Schema Entry Form fields have the same title', function () {
@@ -148,4 +176,69 @@ describe('ase.schemas:Schemas', function () {
             message: jasmine.stringMatching('Diff')
         }]));
     });
+
+    // NOTE: This test must be updated when new field types are added.
+    it('should preserve all information when serializing and deserializing schemas', function () {
+        // In practice this means that the serialize and deserialize methods are inverses
+        var f = Schemas.definitionFromSchemaFormData;
+        var b = Schemas.schemaFormDataFromDefinition;
+
+        var schemaFormData = [{
+            fieldType: 'selectlist',
+            isSearchable: false,
+            displayType: 'checkbox',
+            isRequired: false,
+            fieldTitle: 'Select',
+            fieldOptions: ['opt1', 'opt2'],
+            propertyOrder: 0 // The serializer uses the ordering of the incoming form data to
+                             // determine and set the propertyOrder value, since this is how
+                             // JSON-Editor expresses the value. Therefore this value must match
+                             // the ordering of the array in order to ensure that the output
+                             // matches the input, otherwise the serializer will assume that the
+                             // ordering has changed and update the propertyOrder field, causing
+                             // the test to fail.
+        },{
+            fieldType: 'image',
+            isSearchable: false,
+            isRequired: true,
+            fieldTitle: 'Image',
+            propertyOrder: 1
+        },{
+            fieldType: 'text',
+            isSearchable: false,
+            isRequired: false,
+            fieldTitle: 'Text',
+            textOptions: 'datetime',
+            propertyOrder: 2
+        }];
+
+        // Transform back and forth a few times
+        var output = b(f(b(f(b(f(b(f(b(f(b(f(schemaFormData))))))))))));
+        expect(output).toEqual(schemaFormData);
+    });
+
+    it('should sort by propertyOrder when deserializing schemas', function () {
+        var definition = {
+            properties: {
+                appearsSecond: {
+                    fieldType: 'text',
+                    propertyOrder: 1
+                },
+                appearsThird: {
+                    fieldType: 'text',
+                    propertyOrder: 2
+                },
+                appearsFirst: {
+                    fieldType: 'text',
+                    propertyOrder: 0
+                }
+            }
+        };
+
+        var formData = Schemas.schemaFormDataFromDefinition(definition);
+        for (var i = 1; i < formData.length; i++) {
+            expect(formData[i].propertyOrder).toBeGreaterThan(formData[i-1].propertyOrder);
+        }
+    });
+
 });
