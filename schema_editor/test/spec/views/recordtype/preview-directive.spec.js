5f04d0c7a9dbebc2fff3db40ddef8648dd81c25b
Kenny Shepard <kshepard@gmail.com>
2015-06-03 09:00:18 -0400

    Add preview tests

---
+++ schema_editor/test/spec/views/recordtype/preview-directive.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('ase.views.recordtype: RTPreview', function () {
+
+    beforeEach(module('ase.mock.resources'));
+    beforeEach(module('ase.templates'));
+    beforeEach(module('ase.views.recordtype'));
+
+    var $compile;
+    var $httpBackend;
+    var $rootScope;
+    var RecordTypes;
+    var ResourcesMock;
+
+    beforeEach(inject(function (_$compile_, _$httpBackend_, _$rootScope_,
+                                _RecordTypes_, _ResourcesMock_) {
+        $compile = _$compile_;
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
+        RecordTypes = _RecordTypes_;
+        ResourcesMock = _ResourcesMock_;
+    }));
+
+    it('should load directive', function () {
+        var requestUrl = /\/api\/recordtypes/;
+        $httpBackend.expectGET(requestUrl).respond(200, ResourcesMock.RecordTypeResponse);
+
+        var scope = $rootScope.$new();
+        var element = $compile('<ase-rt-preview></ase-rt-preview>')(scope);
+        $rootScope.$apply();
+
+        expect(element.find('json-editor').length).toEqual(1);
+    });
+});
