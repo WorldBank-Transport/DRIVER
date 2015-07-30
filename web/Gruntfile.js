35f079f69fc4008e32c90013d25cf5feca1c4ea1
Kenny Shepard <kshepard@gmail.com>
2015-07-30 15:22:18 -0400

    Allow jshint to run on all files

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -127,7 +127,7 @@ module.exports = function (grunt) {
       all: {
         src: [
           'Gruntfile.js',
-          '<%= yeoman.app %>/scripts/{,*/}*.js'
+          '<%= yeoman.app %>/scripts/**/*.js'
         ]
       },
       test: {
