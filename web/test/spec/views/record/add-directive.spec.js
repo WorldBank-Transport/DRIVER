378264b0b2fdd6e238fbbfbacdd3b532cdac2a0a
Kenny Shepard <kshepard@gmail.com>
2015-07-30 18:52:53 -0400

    Add some tests

--- web/test/spec/views/record/add-directive.spec.js
+++ web/test/spec/views/record/add-directive.spec.js
@@ -2,22 +2,34 @@
 
 describe('driver.views.record: RecordAdd', function () {
 
+    beforeEach(module('ase.mock.resources'));
+    beforeEach(module('ase.resources'));
+    beforeEach(module('driver.views.record'));
+    beforeEach(module('driver.templates'));
+
     var $compile;
     var $httpBackend;
     var $rootScope;
+    var RecordTypes;
+    var ResourcesMock;
 
-    beforeEach(inject(function (_$compile_, _$httpBackend_, _$rootScope_) {
+    beforeEach(inject(function (_$compile_, _$httpBackend_, _$rootScope_,
+                                _RecordTypes_, _ResourcesMock_) {
         $compile = _$compile_;
         $httpBackend = _$httpBackend_;
         $rootScope = _$rootScope_;
+        RecordTypes = _RecordTypes_;
+        ResourcesMock = _ResourcesMock_;
     }));
 
     it('should load directive', function () {
+        var requestUrl = /\/api\/recordtypes/;
+        $httpBackend.expectGET(requestUrl).respond(200, ResourcesMock.RecordTypeResponse);
+
         var scope = $rootScope.$new();
-        var element = $compile('<driver-record></driver-record>')(scope);
+        var element = $compile('<driver-record-add></driver-record-add>')(scope);
         $rootScope.$apply();
 
-        // TODO: this is a placeholder test
-        expect(1).toEqual(1);
+        expect(element.find('json-editor').length).toEqual(1);
     });
 });
