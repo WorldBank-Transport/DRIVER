d96122e2bf73e8e9c908643d87ed38abe2883340
Kathryn Killebrew <kkillebrew@azavea.com>
2015-08-24 11:42:00 -0400

    Use EPSG3857 everywhere.
    Django/database points in 3857, but data loader and map were in 4326.

--- windshaft/server.js
+++ windshaft/server.js
@@ -8,21 +8,41 @@ var dbPassword = process.env.DRIVER_DB_PASSWORD;
 var redisHost = process.env.DRIVER_REDIS_HOST;
 var redisPort = process.env.DRIVER_REDIS_PORT;
 
+// RFC4122. See: http://stackoverflow.com/questions/7905929/how-to-test-valid-uuid-guid
+var uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+var baseQuery = ["(SELECT * FROM ashlar_record WHERE schema_id = ",
+                 "(SELECT uuid FROM ashlar_recordschema ",
+                 "WHERE next_version_id IS NULL AND record_type_id = '"
+                ].join("");
+
 var config = {
         useProfiler: true,
-        base_url: '/tiles/database/:dbname/table/:table',
-        base_url_notable: '/tiles/database/:dbname',
+        base_url: '/tiles/recordtype/:recordtype',
+        base_url_notable: '/tiles/recordtype/:recordtype',
         req2params: function(req, callback) {
             try {
-                /* CAN SET HERE THINGS LIKE:
-                req.params.table
-                req.params.dbname
+                // check for a valid record type UUID
+                 if (!uuidRegex.test(req.params.recordtype)) {
+                    console.error('invalid record type:');
+                    console.error(req.params.recordtype);
+                    throw('Invalid recordtype UUID');
+                 }
+
+                req.params.dbname = 'driver';
+                req.params.table = 'ashlar_record';
+                req.params.sql = baseQuery + req.params.recordtype + "')) AS ashlar_record";
+                console.log(req.params.sql);
+
+                /* TODO: set
                 req.params.style
                 req.params.interactivity
                 */
+
                 callback(null, req);
             } catch(err) {
                 console.error('req2params error: ');
+                console.error(err);
                 callback(err, null);
             }
         },
@@ -32,8 +52,9 @@ var config = {
             password: dbPassword,
             host: dbHost,
             port: dbPort,
-            geometry_field: 'the_geom',
-            srid: 4326
+            geometry_field: 'geom',
+            // this must match the ashlar SRID set in app/driver/settings.py
+            srid: 3857
           }
         }, //see grainstore npm for other options
         renderCache: {
