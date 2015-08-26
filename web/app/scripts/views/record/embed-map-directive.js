630a319e64ff35bbd391157086c24c327a94f377
Kathryn Killebrew <kkillebrew@azavea.com>
2015-08-26 11:51:51 -0400

    Add draggable location marker to embedded map

--- web/app/scripts/views/record/embed-map-directive.js
+++ web/app/scripts/views/record/embed-map-directive.js
@@ -3,7 +3,7 @@
 
     var stamenTonerAttribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.';
 
-    function driverEmbedMap(Config) {
+    function driverEmbedMap() {
         var module = {
             restrict: 'A',
             scope: false,
@@ -13,6 +13,9 @@
         };
         return module;
 
+        var locationMarker = null;
+        var location = null;
+
         function link(scope, element, attrs, controller) {
             controller.getMap().then(setRecordLayers);
         }
@@ -21,6 +24,19 @@
             var streets = new L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
                                           {attribution: stamenTonerAttribution});
             map.addLayer(streets, {detectRetina: true});
+
+            map.on('click', function(e) {
+                location = [e.latlng.lat, e.latlng.lng];
+                if (locationMarker) {
+                    locationMarker.setLatLng(e.latlng);
+                } else {
+                    locationMarker = new L.marker(e.latlng, {draggable: true}).addTo(map);
+                    locationMarker.on('dragend', function(e) {
+                        var latlng = locationMarker.getLatLng();
+                        location = [latlng.lat, latlng.lng];
+                    });
+                }
+            });
         }
     }
 
