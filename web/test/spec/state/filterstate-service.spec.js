bda226820dff3043cc399e57099319c970480ecf
Kathryn Killebrew <kkillebrew@azavea.com>
2015-09-16 13:06:21 -0400

    Add test for filters storage

--- web/test/spec/state/filterstate-service.spec.js
+++ web/test/spec/state/filterstate-service.spec.js
@@ -12,10 +12,20 @@ describe('driver.state: Records', function () {
         $rootScope = _$rootScope_;
         FilterState = _FilterState_;
         LocalStorageService = _localStorageService_;
+
+        spyOn($rootScope, '$broadcast');
     }));
 
     it('should have a local storage provider', function () {
         expect(LocalStorageService).toBeDefined();
+    });
+
+    it('should load back saved filters', function () {
+        var filter = {'foo#bar': 'baz', 'amplifier': 11};
+        FilterState.saveFilters(filter);
+        expect(LocalStorageService.get('filterbar.filters')).toEqual(JSON.stringify(filter));
+        FilterState.restoreFilters();
+        expect($rootScope.$broadcast).toHaveBeenCalledWith('driver.filterbar:restore', filter);
     });
 
 });
