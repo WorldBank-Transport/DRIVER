b9a13610b6206f5ea3732a976f24dc839bcc9e94
Kathryn Killebrew <kkillebrew@azavea.com>
2015-11-05 18:36:39 -0500

    Enable token authentication

--- app/driver/settings.py
+++ app/driver/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
     'oauth2_provider',
 
     'django_extensions',
@@ -148,6 +149,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
@@ -191,9 +194,6 @@ ASHLAR = {
 }
 
 ## Tweak settings depending on deployment target
-if DEVELOP:
-    # Disable on production, this is for the browseable API only
-    REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] += ('rest_framework.authentication.SessionAuthentication',)
 
 if PRODUCTION:
     # Enabled on production? This allows locking write permissions on views
