6b64bd793930ea9ae1d3ed5c3a0555c2391678d8
Kathryn Killebrew <kkillebrew@azavea.com>
2015-12-17 16:36:47 -0500

    Explicitly delete saved filters on user delete.
    Fixes #349.

--- app/user_filters/models.py
+++ app/user_filters/models.py
@@ -9,5 +9,5 @@ class SavedFilter(models.Model):
     """A filter saved by the user for later reference"""
     uuid = models.UUIDField(primary_key=True, default=uuidlib.uuid4, editable=False)
     label = models.CharField(max_length=255)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     filter_json = jfields.JsonField()
