cd67df39b293bd2f3d14d16bfca898cdcdd59d55
Kathryn Killebrew <kkillebrew@azavea.com>
2015-12-17 13:31:02 -0500

    Display descriptive user creation errors in HTML alert

--- schema_editor/app/scripts/views/usermgmt/add-controller.js
+++ schema_editor/app/scripts/views/usermgmt/add-controller.js
@@ -18,10 +18,32 @@
                                     timeout: 3000});
                 $state.go('usermgmt.list');
             }, function(error) {
-                Notifications.show({text: 'Failed to create user',
-                                    displayClass: 'alert-danger'});
                 $log.error('error creating user:');
                 $log.error(error);
+
+                var errorHtml = '<h4>Failed to create user</h4>';
+
+                // get back dict of {fieldName: [Array of errors]}
+                if (error.data) {
+                    errorHtml += '<ul>';
+                    angular.forEach(error.data, function(fieldErrors, fieldName) {
+                        // list point for each field
+                        errorHtml += '<li>' + fieldName + ':';
+                        if (fieldErrors.length) {
+                            errorHtml += '<ul>';
+                            // sub-list with points for each field error
+                            fieldErrors.forEach(function(err) {
+                                errorHtml += '<li>' + err + '</li>';
+                            });
+                            errorHtml += '</ul>';
+                        }
+                        errorHtml += '</ul>';
+                    });
+                }
+
+                $log.debug(errorHtml);
+
+                Notifications.show({html: errorHtml, displayClass: 'alert-danger'});
             });
         };
     }
