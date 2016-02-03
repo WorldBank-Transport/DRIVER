197471704038c5c6148e49dd4e9daf3caa806e40
Klaas Hoekema <khoekema@azavea.com>
2016-02-03 13:48:14 -0500

    Modify RecordDuplicate model
    * Changes the connection to records from many-to-many to a pair of
    foreign keys
    * Makes it an AshlarModel to get 'created' and 'modified' attributes
    * Adds a score attribute

---
+++ app/data/migrations/0004_modify_recordduplicate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ashlar', '0021_add_weather_fields'),
+        ('data', '0003_dedupejob_recordduplicate'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='recordduplicate',
+            name='records',
+        ),
+        migrations.AddField(
+            model_name='recordduplicate',
+            name='created',
+            field=models.DateTimeField(default=datetime.datetime(2016, 2, 3, 18, 46, 27, 456980, tzinfo=utc), auto_now_add=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='recordduplicate',
+            name='duplicate_record',
+            field=models.ForeignKey(related_name='duplicate_record', to='ashlar.Record', null=True),
+        ),
+        migrations.AddField(
+            model_name='recordduplicate',
+            name='modified',
+            field=models.DateTimeField(default=datetime.datetime(2016, 2, 3, 18, 46, 39, 255953, tzinfo=utc), auto_now=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='recordduplicate',
+            name='record',
+            field=models.ForeignKey(related_name='record', to='ashlar.Record', null=True),
+        ),
+        migrations.AddField(
+            model_name='recordduplicate',
+            name='score',
+            field=models.FloatField(default=0),
+        ),
+    ]
