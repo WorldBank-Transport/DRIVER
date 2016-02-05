9b4c470731c11b57071190ffb2078268008f7757
Lucien Knechtli <lknechtli@azavea.com>
2016-02-05 15:03:14 -0500

    Misc fixes to dedupe celery task
    * Add celery task uuid field to DedupeJob
    * Use created date when finding Records to dedupe, and occurred_from for
      the actual dedupe
    * Move on_success and on_failure proccessing for a task run to a
      DedupeTask class
    * Rename calculate_score to calculate_similarity_score
    * Refactor functions that use time_allowance and distance_allowance to
      take them as arguments instead of making a function call
    * Refactor task to take time_allowance and distance_allowance as
      parameters with default values
    * Remove list() wrapper for queryset.values_list calls
    * Update docstring for get_dedupe_ids
    * Remove try/catch around task so if it fails, it fails. Processing in
      case of failure is handled in on_failure of parent
    * Move logic preventing multiple identical duplicates to
      find_duplicates_for_record to reduce database queries
    * Rename some test variables to make more sense
    * Use celery to run job in tests instead of calling method directly
    * Add tests for helper methods

--- app/data/tasks/deduplicate_records.py
+++ app/data/tasks/deduplicate_records.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from celery import shared_task
+from celery.app.task import Task
 from celery.utils.log import get_task_logger
 
 from data.models import (DedupeJob, RecordDuplicate)
@@ -13,53 +15,64 @@ from pytz import timezone
 logger = get_task_logger(__name__)
 
 
-def get_time_allowance():
-    return datetime.timedelta(hours=settings.DEDUPE_TIME_RANGE_HOURS)
+class DedupeTask(Task):
+    """Base class for the deduplication task to handle updating status in database"""
+
+    def on_success(self, retval, task_id, args, kwargs):
+        job = DedupeJob.objects.get(celery_task=task_id)
+        job.status = DedupeJob.Status.SUCCESS
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        """ On failure, delete duplicates found in the unsuccessful run"""
+        job = DedupeJob.objects.get(celery_task=task_id)
+        job.status = DedupeJob.Status.ERROR
+        RecordDuplicate.objects.filter(job=job).delete()
 
 
-# .001 degrees is ~110m give or take 10 depending on location, which should
-# be good enough
-def get_distance_allowance():
-    return settings.DEDUPE_DISTANCE_DEGREES
-
-
-def calculate_score(record, duplicate):
+def calculate_similarity_score(record1, record2, time_allowance, distance_allowance):
     """
     Set a score representing the likelyhood the two records are duplicated, based
     on the criteria.  score is a float between 0 and 1
 
     normalized dimensions are considered equal (1 day equally as important as 100m)
     """
-    tdelta = abs(record.occurred_from - duplicate.occurred_from)
+    tdelta = abs(record1.occurred_from - record2.occurred_from)
     tscore = 0
     if tdelta.total_seconds() == 0:
         tscore = 1.0
     else:
-        tscore = tdelta.total_seconds() / get_time_allowance().total_seconds()
-    ddelta = record.geom.distance(duplicate.geom)
+        tscore = tdelta.total_seconds() / time_allowance.total_seconds()
+    ddelta = record1.geom.distance(record2.geom)
     dscore = 0
     if ddelta == 0.0:
         dscore = 1.0
     else:
-        dscore = ddelta / get_distance_allowance()
+        dscore = ddelta / distance_allowance
     score = (tscore + dscore) / 2
     return score
 
 
-def find_duplicates_for_record(uuid):
+def find_duplicates_for_record(uuid, time_allowance, distance_allowance):
     """ Given a record, find all possible duplicates for it
     """
     record = Record.objects.get(uuid=uuid)
     return Record.objects.filter(
         schema__record_type=record.schema.record_type,
-        geom__dwithin=(record.geom, get_distance_allowance()),
+        geom__dwithin=(record.geom, distance_allowance),
         occurred_from__range=(
-            record.occurred_from - get_time_allowance(), record.occurred_from + get_time_allowance()
+            record.occurred_from - time_allowance, record.occurred_from + time_allowance
         )
-    ).exclude(uuid=record.uuid)
+    ).exclude(
+        uuid=record.uuid,
+        uuid__in=RecordDuplicate.objects.filter(
+            Q(record__uuid=record.uuid) | Q(
+                duplicate_record__uuid=record.uuid
+            )
+        )
+    )
 
 
-def get_time_extent():
+def get_time_extent(time_allowance):
     """ Get the time range to dedupe
     Calculate from X hr before the last dedupe run to the current task run's
     start time, where X is the heuristic's time allowance
@@ -69,15 +82,14 @@ def get_time_extent():
         {starttime: timestamp, endtime: timestamp}
     """
     # TODO: get config var determining matching time - use value of 1 hr for now
-    time_allowance = get_time_allowance()
     last_job = None
     if DedupeJob.objects.all().count() > 0:
         last_job = DedupeJob.objects.latest()
     start_time = None
     if last_job:
-        start_time = last_job.datetime - time_allowance
+        start_time = last_job.datetime
     else:
-        start_time = Record.objects.earliest('occurred_from').occurred_from
+        start_time = Record.objects.earliest('created').created
     end_time = datetime.datetime.utcnow().replace(tzinfo=timezone('UTC'))
     return {'start_time': start_time, 'end_time': end_time}
 
@@ -88,17 +100,17 @@ def get_dedupe_set(extent):
     generate it
     """
     queryset = Record.objects.filter(
-        occurred_from__range=(
+        created__range=(
             extent['start_time'], extent['end_time']
         )
     ).exclude(uuid__in=get_dedupe_ids())
-    ids = list(queryset.values_list(queryset.model._meta.pk.name, flat=True))
+    ids = queryset.values_list('uuid', flat=True)
     return (ids, queryset)
 
 
 def get_dedupe_ids(job=None):
-    """ Get the ids of records to dedupe
-    return a set of uuids to check for duplicates.
+    """
+    Gets the set of RecordDuplicates for the specified job, or all of them if none specified
     """
     queryset = None
     if job:
@@ -109,8 +121,8 @@ def get_dedupe_ids(job=None):
     return ids
 
 
-@shared_task
-def find_duplicate_records():
+@shared_task(base=DedupeTask, bind=True)
+def find_duplicate_records(self, time_allowance=None, distance_allowance=None, task_id=None):
     """ Find all duplicate records
     Duplicate records are classified as two records that occured at the same time
     and location
@@ -131,55 +143,47 @@ def find_duplicate_records():
     if the celery worker itself goes down.  Otherwise, the task status should be
     set properly
     """
-    time_extent = get_time_extent()
+    if time_allowance is None:
+        time_allowance = datetime.timedelta(hours=settings.DEDUPE_TIME_RANGE_HOURS)
+    if distance_allowance is None:
+        distance_allowance = settings.DEDUPE_DISTANCE_DEGREES
+
+    time_extent = get_time_extent(time_allowance)
     ids, queryset = get_dedupe_set(time_extent)
     # this can be adjusted depending on the memory required
     paginator = Paginator(ids, 1000)
-    job = DedupeJob(status=DedupeJob.Status.STARTED)
+
+    job = DedupeJob(status=DedupeJob.Status.STARTED, celery_task=self.request.id)
     job.save()
-    starttime = datetime.datetime.now()
-    duplicatecount = 0
-    try:
-        logger.info("Finding duplicates. 1000 records/batch, %d batches total" %
-                    (paginator.num_pages))
-        for page_num in paginator.page_range:
-            page = paginator.page(page_num)
-            records = queryset.in_bulk(page.object_list)
-            done = []
-            for record in records:
-                done.append(record)
-                duplicates = find_duplicates_for_record(record)
-                recordDuplicates = []
-                for duplicate in duplicates:
-                    if duplicate.uuid not in done:
-                        # handle this in the find_duplicates_for_record call
-                        # to reduce number of queries
-                        num_reversed = RecordDuplicate.objects.filter(
-                            record__uuid=duplicate.uuid,
-                            duplicate_record__uuid=record
-                        ).count()
-                        if num_reversed == 0:
-                            rec = Record.objects.get(uuid=record)
-                            recordDuplicates.append(
-                                RecordDuplicate(
-                                    record=rec,
-                                    duplicate_record=duplicate,
-                                    job=job,
-                                    score=calculate_score(rec, duplicate)
-                                )
+
+    start_time = datetime.datetime.now()
+    duplicate_count = 0
+    logger.info("Finding duplicates. 1000 records/batch, %d batches total" % (paginator.num_pages))
+    for page_num in paginator.page_range:
+        page = paginator.page(page_num)
+        records = queryset.in_bulk(page.object_list)
+        done = []
+        for rec_id, record in records.iteritems():
+            done.append(rec_id)
+            duplicates = find_duplicates_for_record(rec_id, time_allowance, distance_allowance)
+            record_duplicates = []
+            for duplicate in duplicates:
+                if duplicate.uuid not in done:
+                    record_duplicates.append(
+                        RecordDuplicate(
+                            record=record,
+                            duplicate_record=duplicate,
+                            job=job,
+                            score=calculate_similarity_score(
+                                record, duplicate, time_allowance, distance_allowance
                             )
-                            duplicatecount += 1
-                RecordDuplicate.objects.bulk_create(recordDuplicates)
-            logger.info(
-                "Batch %d of %d done. %d duplicates found, %s elapsed" % (
-                    page_num, paginator.num_pages, duplicatecount,
-                    datetime.datetime.now() - starttime
-                )
+                        )
+                    )
+                    duplicate_count += 1
+            RecordDuplicate.objects.bulk_create(record_duplicates)
+        logger.info(
+            "Batch %d of %d done. %d duplicates found, %s elapsed" % (
+                page_num, paginator.num_pages, duplicate_count, datetime.datetime.now() - start_time
             )
-        logger.info("Finished searching for duplicate records.")
-        job.status = DedupeJob.Status.SUCCESS
-    except Exception as e:
-        # TODO: retry job next time it runs with same start time
-        job.status = DedupeJob.Status.ERROR
-        logger.error("Failed: exception=\n%s" % (e, ))
-    job.save()
+        )
+    logger.info("Finished searching for duplicate records.")
