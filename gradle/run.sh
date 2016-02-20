6870e62d1142678e7b89e78eefcd0ff066fb95f5
Kathryn Killebrew <banderkat@gmail.com>
2016-02-20 16:37:35 -0500

    Verify jar after build; check for clases.dex

--- gradle/run.sh
+++ gradle/run.sh
@@ -15,18 +15,20 @@ TSA_URL="http://timestamp.digicert.com"
 # name of json file gets used by jsonschema2pojo for the base class
 DRIVER_SCHEMA_FILE="DriverSchema.json"
 
-# register the cleanup function to be called on the EXIT signal
+# clean up temporary files on exit
 trap "echo 'Cleaning up...'; rm $MODEL_JAR; rm -rf $SCHEMA_SOURCE_DIR; echo 'All done!'" EXIT
 
+set -e
+
 # delete redis key for schema, if it exists
-redis-cli -h $DRIVER_REDIS_HOST -p $DRIVER_REDIS_PORT -n $DRIVER_REDIS_JAR_DB DEL ${SCHEMA_UUID}
+redis-cli -h $DRIVER_REDIS_HOST -p $DRIVER_REDIS_PORT -n $DRIVER_REDIS_JAR_DB DEL $1
 
 echo "Going to build new model jarfile ${MODEL_JAR}..."
 
 echo "$2" > $SCHEMA_SOURCE_DIR/${DRIVER_SCHEMA_FILE}
 
-gradle clean assemble dexify --project-prop outfile=$MODEL_JAR \
-    --project-prop sourcedir=$SCHEMA_SOURCE_DIR
+gradle -Dorg.gradle.native=false clean assemble dexify --project-prop outfile=$MODEL_JAR \
+    --project-prop sourcedir=$SCHEMA_SOURCE_DIR --stacktrace --info
 
 echo 'Model jarfile built.'
 
@@ -42,10 +44,21 @@ jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$ANDROID_KEYST
 # verfiy JAR is signed
 jarsigner -verify -certs -keystore "$ANDROID_KEYSTORE" "$MODEL_JAR"
 
+# verify jar integrity
+zip -T "$MODEL_JAR"
+
+# Check jar has a classes.dex in it. If the schema json input was not found, or if
+# jsonschema2pojo could not parse it, the gradle build will succeed, but the output jar
+# will only contain META-INF.
+if [ `zip -sf "$MODEL_JAR" | grep classes.dex | wc -l` != 1 ]; then
+    echo "Built jar file has no classes.dex. Is the schema json present and valid?"
+    exit -2
+fi
+
 # put jar file contents into redis
 redis-cli -h $DRIVER_REDIS_HOST -p $DRIVER_REDIS_PORT -n $DRIVER_REDIS_JAR_DB -x SET $1 < $MODEL_JAR
 
 # set expiry on key
-redis-cli -h $DRIVER_REDIS_HOST -p $DRIVER_REDIS_PORT -n $DRIVER_REDIS_JAR_DB EXPIRE $3
+redis-cli -h $DRIVER_REDIS_HOST -p $DRIVER_REDIS_PORT -n $DRIVER_REDIS_JAR_DB EXPIRE $1 $3
 
 echo "Jar contents set in redis for schema ${1}. All done!"
