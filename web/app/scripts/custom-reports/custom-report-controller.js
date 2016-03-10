b29e0fcf80b6d1527a97dc840305b32a135a9dc4
Klaas Hoekema <khoekema@azavea.com>
2016-03-10 14:05:25 -0500

    Add row category label and table labels

--- web/app/scripts/custom-reports/custom-report-controller.js
+++ web/app/scripts/custom-reports/custom-report-controller.js
@@ -2,14 +2,38 @@
     'use strict';
 
     /* ngInject */
-    function CustomReportController($state, $stateParams, Records) {
+    function CustomReportController($state, $stateParams, Records, AggregationsConfig) {
         var ctl = this;
+        ctl.dateFormat = 'MMM D, YYYY';
         init();
 
         function init() {
-            var params = $stateParams;
-            Records.report(params).$promise.then(function (report) {
-                ctl.report = report;
+            ctl.params = $stateParams;
+
+            getCategoryLabels().then(function () {
+                Records.report(ctl.params).$promise.then(function (report) {
+                    ctl.report = report;
+                });
+            });
+        }
+
+        function getCategoryLabels() {
+            return AggregationsConfig.getOptions().then(function (options) {
+                /* jshint camelcase: false */
+                var rowCategory = _.find(options, function (filter) {
+                    return [ctl.params.row_period_type,
+                            ctl.params.row_boundary_id,
+                            ctl.params.row_choices_path].indexOf(filter.value) >= 0;
+                    });
+                ctl.rowCategoryLabel = rowCategory.label;
+
+                var colCategory = _.find(options, function (filter) {
+                    return [ctl.params.col_period_type,
+                            ctl.params.col_boundary_id,
+                            ctl.params.col_choices_path].indexOf(filter.value) >= 0;
+                    });
+                ctl.colCategoryLabel = colCategory.label;
+                /* jshint camelcase: true */
             });
         }
     }
