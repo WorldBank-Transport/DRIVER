5d7a6b765693276b1292f4810ddef001ae07b9d2
Derek Dohler <ddohler@azavea.com>
2016-04-14 15:52:01 -0400

    Add function to download OSM data

--- app/black_spots/tasks/__init__.py
+++ app/black_spots/tasks/__init__.py
@@ -4,5 +4,4 @@ from celery import shared_task, Task
 from celery.utils.log import get_task_logger
 
 from black_spots.tasks.calculate_black_spots import calculate_black_spots
-
-
+from black_spots.tasks.load_road_network import load_road_network
