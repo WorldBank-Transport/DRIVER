0a029be03838bc7a603f15109994f6a14fd644a1
Christopher Beddow <christopher@mapillary.com>
2017-02-23 14:43:58 +0100

    Add Mapillary viewer to record popup

--- web/app/scripts/views/map/layers-controller.js
+++ web/app/scripts/views/map/layers-controller.js
@@ -725,8 +725,21 @@
             // add header with record date constant field
             /* jshint camelcase: false */
             // DateTimes come back from Windshaft without tz information, but they're all UTC
+            var url = "https://a.mapillary.com/v3/images?lookat=" + e.latlng.lng + "," + e.latlng.lat + "&closeto=" + e.latlng.lng + "," + e.latlng.lat + "&client_id=UTZhSnNFdGpxSEFFREUwb01GYzlXZzo5ZWIxZWYxNzM1YWY2MjNm&radius=400"
+            var imgkey
+            var mly
+            $.ajax({
+                      dataType: 'json',
+                      url: url,
+                      success: function (data) {
+                  imgkey = data.images[0]["key"]
+                  mly = '<iframe width=\"320\" height=\"200\" src=\"https://embed-v1.mapillary.com/embed?show_segmentation=true&version=1&filter=%5B%22all%22%5D&map_filter=%5B%22all%22%5D&image_key=" + imgkey + "&client_id=UTZhSnNFdGpxSEFFREUwb01GYzlXZzo5ZWIxZWYxNzM1YWY2MjNm&style=photo\" frameborder=\"0\"></iframe>'
+                  }
+
+            });
             var occurredStr = localizeRecordDateFilter(moment.utc(record.occurred_from), dateFormat, true);
             var str = '<div id="record-popup" class="record-popup">';
+            str += mly;
             str += '<div><h5>' + popupParams.label + ' ' + detailsLabel +
                 '</h5><h3>' + occurredStr + '</h3>';
             /* jshint camelcase: true */
