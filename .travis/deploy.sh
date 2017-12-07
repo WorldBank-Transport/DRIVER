0e38885e4bf1d1b1f04e1187f16ac99dbdec2970
Derek Dohler <ddohler@azavea.com>
2017-12-07 11:33:06 -0500

    Disable Travis staging deployments
    We no longer have a staging environment.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,28 +10,30 @@ do
   docker push "quay.io/azavea/driver-${image}:latest"
 done
 
+# TODO: The below was for deploying to our staging environment, which we no longer have.
+# If staging is ever resurrected, we can re-enable this.
 # Set up group vars
-echo "setting up group vars..."
-set +x
-grep -v nominatim_key deployment/ansible/group_vars/all.example \
-    > deployment/ansible/group_vars/all
-echo "web_js_nominatim_key: \"${NOMINATIM_API_KEY}\"" \
-    >> deployment/ansible/group_vars/all
-echo "oauth_client_id: \"${OAUTH_CLIENT_ID}\"" \
-    >> deployment/ansible/group_vars/all
-echo "oauth_client_secret: \"${OAUTH_CLIENT_SECRET}\"" \
-    >> deployment/ansible/group_vars/all
-echo "forecast_io_api_key: \"${FORECAST_IO_API_KEY}\"" \
-    >> deployment/ansible/group_vars/all
-echo "keystore_password: \"${DRIVER_KEYSTORE_PASSWORD}\"" \
-    >> deployment/ansible/group_vars/all
-set -x
+#echo "setting up group vars..."
+#set +x
+#grep -v nominatim_key deployment/ansible/group_vars/all.example \
+#    > deployment/ansible/group_vars/all
+#echo "web_js_nominatim_key: \"${NOMINATIM_API_KEY}\"" \
+#    >> deployment/ansible/group_vars/all
+#echo "oauth_client_id: \"${OAUTH_CLIENT_ID}\"" \
+#    >> deployment/ansible/group_vars/all
+#echo "oauth_client_secret: \"${OAUTH_CLIENT_SECRET}\"" \
+#    >> deployment/ansible/group_vars/all
+#echo "forecast_io_api_key: \"${FORECAST_IO_API_KEY}\"" \
+#    >> deployment/ansible/group_vars/all
+#echo "keystore_password: \"${DRIVER_KEYSTORE_PASSWORD}\"" \
+#    >> deployment/ansible/group_vars/all
+#set -x
 
-ansible-galaxy install -f -r deployment/ansible/roles.yml -p deployment/ansible/roles
+#ansible-galaxy install -f -r deployment/ansible/roles.yml -p deployment/ansible/roles
 
-chmod 0600 deployment/driver.pem
+#chmod 0600 deployment/driver.pem
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-  -i deployment/ansible/inventory/staging \
-  --private-key="${TRAVIS_BUILD_DIR}/deployment/driver.pem" \
-  deployment/ansible/database.yml deployment/ansible/app.yml deployment/ansible/celery.yml
+#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+#  -i deployment/ansible/inventory/staging \
+#  --private-key="${TRAVIS_BUILD_DIR}/deployment/driver.pem" \
+#  deployment/ansible/database.yml deployment/ansible/app.yml deployment/ansible/celery.yml
