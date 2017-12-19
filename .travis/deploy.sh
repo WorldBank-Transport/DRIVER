327cefaef0ef5e5b14914d5a38560bc754f0b791
Derek Dohler <ddohler@azavea.com>
2017-12-19 10:15:24 -0500

    Support tagged releases in Travis, Ansible
    This configures Travis to deploy Docker containers with tags matching
    the built tag on the master branch. It also adds an Ansible variable
    called `app_version` that can be used to specify which Docker container
    gets deployed when running production deployments. In development, tags
    are not used, only `latest` is used.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,35 +5,7 @@ set -x
 
 for image in app editor web gradle analysis;
 do
-  docker push "quay.io/azavea/driver-${image}:${TRAVIS_COMMIT:0:7}"
-  docker tag "quay.io/azavea/driver-${image}:${TRAVIS_COMMIT:0:7}" "quay.io/azavea/driver-${image}:latest"
-  docker push "quay.io/azavea/driver-${image}:latest"
+  docker push "quay.io/azavea/driver-${image}:${TRAVIS_TAG}"
+  docker tag "quay.io/azavea/driver-${image}:${TRAVIS_TAG}" "quay.io/azavea/driver-${image}:${TRAVIS_TAG}"
+  docker push "quay.io/azavea/driver-${image}:${TRAVIS_TAG}"
 done
-
-# TODO: The below was for deploying to our staging environment, which we no longer have.
-# If staging is ever resurrected, we can re-enable this.
-# Set up group vars
-#echo "setting up group vars..."
-#set +x
-#grep -v nominatim_key deployment/ansible/group_vars/all.example \
-#    > deployment/ansible/group_vars/all
-#echo "web_js_nominatim_key: \"${NOMINATIM_API_KEY}\"" \
-#    >> deployment/ansible/group_vars/all
-#echo "oauth_client_id: \"${OAUTH_CLIENT_ID}\"" \
-#    >> deployment/ansible/group_vars/all
-#echo "oauth_client_secret: \"${OAUTH_CLIENT_SECRET}\"" \
-#    >> deployment/ansible/group_vars/all
-#echo "forecast_io_api_key: \"${FORECAST_IO_API_KEY}\"" \
-#    >> deployment/ansible/group_vars/all
-#echo "keystore_password: \"${DRIVER_KEYSTORE_PASSWORD}\"" \
-#    >> deployment/ansible/group_vars/all
-#set -x
-
-#ansible-galaxy install -f -r deployment/ansible/roles.yml -p deployment/ansible/roles
-
-#chmod 0600 deployment/driver.pem
-
-#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
-#  -i deployment/ansible/inventory/staging \
-#  --private-key="${TRAVIS_BUILD_DIR}/deployment/driver.pem" \
-#  deployment/ansible/database.yml deployment/ansible/app.yml deployment/ansible/celery.yml
