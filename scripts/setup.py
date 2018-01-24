d6f5dfab418fdb05598dd8197436597573d26aa2
Peter Caisse <pcaisse@gmail.com>
2018-01-23 21:46:28 -0500

    Also write inventory file

--- scripts/setup.py
+++ scripts/setup.py
@@ -7,18 +7,20 @@ import secrets
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 PROJECT_PATH = os.path.abspath(os.path.join(FILE_PATH, os.path.pardir))
-TEMPLATE_PATH = 'deployment/ansible/group_vars'
-TEMPLATE_FILENAME = 'production.j2'
-OUTPUT_FILENAME = 'production'
-
-TEMPLATE_ENVIRONMENT = Environment(
-    autoescape=False,
-    loader=FileSystemLoader(os.path.join(PROJECT_PATH, TEMPLATE_PATH)),
-    trim_blocks=False)
+GROUP_VARS_PATH = 'deployment/ansible/group_vars'
+GROUP_VARS_TEMPLATE_FILENAME = 'production.j2'
+GROUP_VARS_FILENAME = 'production'
+INVENTORY_PATH = 'deployment/ansible/inventory'
+INVENTORY_TEMPLATE_FILENAME = 'production.j2'
+INVENTORY_FILENAME = 'production'
 
 
-def render_template(context):
-    return TEMPLATE_ENVIRONMENT.get_template(TEMPLATE_FILENAME).render(context)
+def render_template(template_path, template_filename, context):
+    template_environment = Environment(
+        autoescape=False,
+        loader=FileSystemLoader(template_path),
+        trim_blocks=False)
+    return template_environment.get_template(template_filename).render(context)
 
 
 def generate_password():
@@ -46,26 +48,39 @@ def wrap_in_quotes(s):
     '--celery_server_ip',
     prompt='Celery server IP',
 )
-def create_production_group_vars(
+def create_files_for_deployment(
         app_domain_name, database_server_ip, app_server_ip, celery_server_ip):
 
-    context = {
-        'app_domain_name': wrap_in_quotes(app_domain_name),
-        'database_server_ip': wrap_in_quotes(database_server_ip),
-        'app_server_ip': wrap_in_quotes(app_server_ip),
-        'celery_server_ip': wrap_in_quotes(celery_server_ip),
-        'postgresql_password': wrap_in_quotes(generate_password()),
-        'windshaft_db_password': wrap_in_quotes(generate_password()),
-        'heimdall_db_password': wrap_in_quotes(generate_password()),
-        'csrf_session_key': wrap_in_quotes(generate_password()),
-        'cookie_secret_key': wrap_in_quotes(generate_password()),
-    }
-
-    with open(os.path.join(TEMPLATE_PATH, OUTPUT_FILENAME), 'w') as f:
-        group_vars = render_template(context)
+    with open(os.path.join(GROUP_VARS_PATH, GROUP_VARS_FILENAME), 'w') as f:
+        group_vars = render_template(
+            os.path.join(PROJECT_PATH, GROUP_VARS_PATH),
+            GROUP_VARS_TEMPLATE_FILENAME,
+            {
+                'database_server_ip': wrap_in_quotes(database_server_ip),
+                'app_server_ip': wrap_in_quotes(app_server_ip),
+                'celery_server_ip': wrap_in_quotes(celery_server_ip),
+                'app_domain_name': wrap_in_quotes(app_domain_name),
+                'postgresql_password': wrap_in_quotes(generate_password()),
+                'windshaft_db_password': wrap_in_quotes(generate_password()),
+                'heimdall_db_password': wrap_in_quotes(generate_password()),
+                'csrf_session_key': wrap_in_quotes(generate_password()),
+                'cookie_secret_key': wrap_in_quotes(generate_password()),
+            }
+        )
         f.write(group_vars)
 
-    # TODO: Write inventory file
+    with open(os.path.join(INVENTORY_PATH, INVENTORY_FILENAME), 'w') as f:
+        inventory = render_template(
+            os.path.join(PROJECT_PATH, INVENTORY_PATH),
+            INVENTORY_TEMPLATE_FILENAME,
+            {
+                'database_server_ip': database_server_ip,
+                'app_server_ip': app_server_ip,
+                'celery_server_ip': celery_server_ip,
+            }
+        )
+        f.write(inventory)
+
 
 if __name__ == '__main__':
-    create_production_group_vars()
+    create_files_for_deployment()
