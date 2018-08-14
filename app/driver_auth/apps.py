8b324a2c8609546afcf6b4f278c5195d83d52956
Peter Caisse <pcaisse@gmail.com>
2018-08-14 09:49:06 -0400

    Move imports for signals and user models inside function scope
    This is to avoid an `AppRegistryNotReady: Apps aren't loaded yet.`
    exception from Django.

--- app/driver_auth/apps.py
+++ app/driver_auth/apps.py
@@ -3,14 +3,13 @@ Override app config to register signal for setting default user group on user cr
 """
 from django.apps import AppConfig
 from django.conf import settings
-from django.db.models.signals import post_save
-from django.contrib.auth.models import Group, User
 
 READ_GROUP = settings.DRIVER_GROUPS['READ_ONLY']
 
 def add_to_default_group(sender, **kwargs):
     user = kwargs["instance"]
     if kwargs["created"]:
+        from django.contrib.auth.models import Group
         group = Group.objects.get(name=READ_GROUP)
         user.groups.add(group)
 
@@ -19,4 +18,6 @@ class DriverConfig(AppConfig):
     name = 'driver_auth'
     verbose_name = 'Authentication and Permissions for DRIVER'
     def ready(self):
+        from django.contrib.auth.models import User
+        from django.db.models.signals import post_save
         post_save.connect(add_to_default_group, sender=User)
