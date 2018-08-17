d246c7c950722459e59542d5bf9621ca4a03af22
Peter Caisse <pcaisse@gmail.com>
2018-08-17 15:49:54 -0400

    Use new syntax for specifying lookup type

--- app/data/filters.py
+++ app/data/filters.py
@@ -5,8 +5,8 @@ from models import RecordAuditLogEntry, RecordDuplicate
 
 class RecordAuditLogFilter(django_filters.FilterSet):
     """Allow filtering audit log entries by user, record, min_date, max_date"""
-    min_date = django_filters.IsoDateTimeFilter(name="date", lookup_type='gte')
-    max_date = django_filters.IsoDateTimeFilter(name="date", lookup_type='lte')
+    min_date = django_filters.IsoDateTimeFilter(name="date", lookup_expr='gte')
+    max_date = django_filters.IsoDateTimeFilter(name="date", lookup_expr='lte')
     action = django_filters.ChoiceFilter(choices=RecordAuditLogEntry.ActionTypes.choices)
 
     class Meta:
