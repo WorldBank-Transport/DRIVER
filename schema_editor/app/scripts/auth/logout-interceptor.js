72ce37f732e3b2e8f5b5749cea196a1fda827b0c
Peter Caisse <pcaisse@gmail.com>
2018-08-24 14:11:59 -0400

    Only log out user if the 401 came from this host
    This is to avoid a bug whereby a third party API (PickPoint) was throwing a 401, resulting
    in the user being logged out.

--- schema_editor/app/scripts/auth/logout-interceptor.js
+++ schema_editor/app/scripts/auth/logout-interceptor.js
@@ -15,13 +15,23 @@
 
         module.events = events;
         module.responseError =  function(response) {
-            if (response.status === 401) {
+            if (response.status === 401 && isSameHost(response.config.url)) {
                 $rootScope.$broadcast(events.logOutUser);
             }
             return $q.reject(response);
         };
 
         return module;
+    }
+
+    /**
+     * Check if the URL has the same host as the current page.
+     */
+    function isSameHost(url) {
+        // Cross-browser way to get the hostname from a URL
+        var urlParser = document.createElement('a');
+        urlParser.href = url;
+        return urlParser.hostname === window.location.hostname;
     }
 
     angular.module('ase.auth').factory('LogoutInterceptor', LogoutInterceptor);
