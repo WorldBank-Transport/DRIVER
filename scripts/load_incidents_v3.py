e8e1e2eb323374f9a57e673db859b545c00729a9
Reed Martz <rmartz@azavea.com>
2019-01-15 16:27:58 -0500

    Extend import data to enable importing data for Brazil

--- scripts/load_incidents_v3.py
+++ scripts/load_incidents_v3.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """Loads incidents from multiple incident database dumps (schema v3)"""
+from contextlib import contextmanager
+from itertools import groupby
+from collections import defaultdict
+from datetime import datetime, timedelta
 import argparse
 import csv
 from dateutil import parser
-import glob
 import logging
 import json
 import os
-from osgeo import ogr, osr
 import pytz
 from time import sleep
 import uuid
@@ -19,6 +22,58 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger()
 
 
+@contextmanager
+def open_many(file_paths, mode='r'):
+    # Allows opening arbitrary number of files in a single context manager
+    handlers = {path: open(path, mode) for path in file_paths}
+    try:
+        yield handlers
+    finally:
+        for fp in handlers.values():
+            fp.close()
+
+
+def merge_sort_files(paths, join_col):
+    # Merges multiple inputs into a stream of ordered tuples of the format (id, source, row)
+    with open_many(paths, 'r') as handlers:
+        readers = {key: csv.DictReader(fp) for key, fp in handlers.items()}
+
+        lines = {}
+        for key, reader in readers.items():
+            try:
+                lines[key] = next(reader)
+            except StopIteration:
+                # If the file has no rows, ignore it
+                pass
+        # Loop over all open files until we've removed them all
+        while lines:
+            # Find the lowest ID in any of the open files
+            join_id = min(line[join_col] for line in lines.values())
+            for key in lines.keys():
+                line = lines[key]
+                while line[join_col] == join_id:
+                    # If the ID matches, send it up and then read the next line from the file
+                    yield (join_id, key, line)
+                    try:
+                        line = next(readers[key])
+                        lines[key] = line
+                    except StopIteration:
+                        # We've reached the end of this file, remove the file from consideration
+                        del lines[key]
+                        break
+
+
+def collate_multiple_files(paths, join_col):
+    merged_stream = merge_sort_files(paths, join_col)
+
+    # Merge the sorted stream of rows into dictionaries per unique ID
+    for id, matches in groupby(merged_stream, lambda row: row[0]):
+        result = defaultdict(list)
+        for id, key, line in matches:
+            result[key].append(line)
+        yield result
+
+
 def extract(csv_path):
     """Simply pulls rows into a DictReader"""
     with open(csv_path) as csvfile:
@@ -27,68 +82,79 @@ def extract(csv_path):
             yield row
 
 
-def transform(record, schema_id):
+def format_record_object(data, mapping):
+    output = {}
+    for csv_key, driver_key, cast_func in mapping:
+        try:
+            value = data[csv_key]
+        except KeyError:
+            continue
+        output[driver_key] = cast_func(value)
+
+    # Add in the _localId field; they're not used here but the schema requires them
+    output['_localId'] = str(uuid.uuid4())
+
+    return output
+
+
+def construct_record_data(record, persons, vehicles):
+    return {
+        'driverIncidentDetails': format_record_object(record, [
+            ('', 'Log1', int),
+            ('', 'Numero', int),
+            ('', 'CodReferencia', int),
+            ('', 'Log2', int),
+            ('', 'Log3', int),
+            ('', 'CodIntersecao', int),
+            ('', 'Jurisdicao', str),
+            ('', 'CodNatureza', int),
+            ('', 'TipoCruzamento', int),
+            ('', 'INTERSEÇÃO?', str),
+            ('', 'Natureza', str)
+        ]),
+        'driverPerson': [format_record_object(person,  [
+            ('', 'CdPessoa', int),
+            ('', 'CdGravidadeLesao', int),
+            ('', 'Sexo', str),
+            ('', 'TipoPessoa', int),
+            ('', 'CdVeiculo', int),
+            ('', 'Idade', int)
+        ]) for person in persons],
+        'driverVehicle': [format_record_object(vehicle,  [
+            ('', 'CdVeiculo', int),
+            ('', 'Ano', int),
+            ('', 'TipoVeiculo', str),
+            ('', 'Linha', int)
+        ]) for vehicle in vehicles]
+    }
+
+
+def transform(record, vehicles, people, schema_id):
     """Converts denormalized rows into objects compliant with the schema.
 
     Doesn't do anything fancy -- if the schema changes, this needs to change too.
     """
-    details_mapping = [
-        ('severity', 'Severity', str),
-        ('description', 'Description', str),
-        ('num_vehicles', 'Num vehicles', int),
-        ('num_driver_casualties', 'Num driver casualties', int),
-        ('num_passenger_casualties', 'Num passenger casualties', int),
-        ('num_pedestrian_casualties', 'Num pedestrian casualties', int),
-        ('traffic_control', 'Traffic control', str),
-        ('collision_type', 'Collision type', str),
-        ('street_lights', 'Street lights', str),
-        ('surface_condition', 'Surface condition', str),
-        ('surface_type', 'Surface type', str),
-        ('main_cause', 'Main cause', str)
-    ]
 
     # Calculate value for the occurred_from/to fields in local time
-    occurred_date = parser.parse(record['record_date'])
-    occurred_date = pytz.timezone('Asia/Manila').localize(occurred_date)
+    occurred_date = parser.parse('{date} {time}'.format(
+        date=record['Data'],
+        time=record['Hora'] if record['Hora'] != 'N' else ''
+    ))
+    occurred_date = pytz.timezone('America/Sao_Paulo').localize(occurred_date)
 
     # Set the geom field
-    # Some of the files use lat/lon, others use 3123.
-    # Reproject the ones that don't look like lat/lon.
-    point = ogr.Geometry(ogr.wkbPoint)
-    if -180 < float(record['lon']) < 180:
-        point.AddPoint(float(record['lon']), float(record['lat']))
-        point.FlattenTo2D()
-    else:
-        point.AddPoint(int(record['lon']), int(record['lat']))
-        point.FlattenTo2D()
-
-        source = osr.SpatialReference()
-        source.ImportFromEPSG(3123)
-
-        target = osr.SpatialReference()
-        target.ImportFromEPSG(4326)
-
-        transform = osr.CoordinateTransformation(source, target)
-        point.Transform(transform)
+    geom = "POINT ({lon} {lat})".format(
+        lon=float(record['Longitude']),
+        lat=float(record['Latitude'])
+    )
 
     obj = {
-        'data': {
-            'driverIncidentDetails': {
-                driver_key: cast_func(record[csv_key])
-                for csv_key, driver_key, cast_func in details_mapping
-                if csv_key in record
-            },
-            'driverPerson': [],
-            'driverVehicle': []
-        },
+        'data': construct_record_data(record, vehicles, people),
         'schema': str(schema_id),
         'occurred_from': occurred_date.isoformat(),
         'occurred_to': occurred_date.isoformat(),
-        'geom': point.ExportToWkt()
+        'geom': geom
     }
-
-    # Add in the _localId field; they're not used here but the schema requires them
-    obj['data']['driverIncidentDetails']['_localId'] = str(uuid.uuid4())
 
     return obj
 
@@ -149,6 +215,7 @@ def main():
     parser.add_argument('--api-url', help='API host / path to target for loading data',
                         default='http://localhost:7000/api')
     parser.add_argument('--authz', help='Authorization header')
+    parser.add_argument('--schema-id', help='UUID for the Record Type to use')
     args = parser.parse_args()
 
     headers = None
@@ -157,22 +224,45 @@ def main():
         headers = {'Authorization': args.authz}
 
     # Do the work
-    schema_id = create_schema(args.schema_path, args.api_url, headers)
+    schema_id = args.schema_id
+    if not schema_id:
+        logger.info("Creating schema remotely")
+        schema_id = create_schema(args.schema_path, args.api_url, headers)
     logger.info("Loading data")
-    count = 1
 
     # Load all files in the directory, ordered by file size
-    files = sorted(glob.glob(args.incidents_csv_dir + '/*.csv'), key=os.path.getsize)
-    logger.info("Files to process: {}".format(files))
+    files = {
+        'record': 'acidentes.csv',
+        'vehicles': 'veiculos.csv',
+        'people': 'vitimas.csv'
+    }
 
-    for csv_file in files:
-        logger.info("Loading file: {}".format(csv_file))
-        for record in extract(csv_file):
-            if count % 100 == 0:
-                logger.info("{0} (file {1} of {2})".format(
-                    count, files.index(csv_file) + 1, len(files)))
-            load(transform(record, schema_id), args.api_url, headers)
-            count += 1
+    logger.info("{} - Importing records".format(datetime.now()))
+    last_print = datetime.now()
+
+    count = 0
+    for record_set in collate_multiple_files(files.values(), 'CdAcidente'):
+        if datetime.now() - last_print > timedelta(minutes=1):
+            logger.info("{} - Imported {} records".format(datetime.now(), count))
+            last_print = datetime.now()
+
+        try:
+            record = record_set[files['record']][0]
+        except KeyError:
+            # Somehow there was a record in one of the addendum files that wasn't in the main file
+            # We don't have enough info to go on, so log the error and skip it
+            logger.warn("Found record join with no associated incident, skipping")
+            continue
+
+        vehicles = record_set.get(files['vehicles'], [])
+        people = record_set.get(files['people'], [])
+
+        record_data = transform(record, vehicles, people, schema_id)
+        print(json.dumps(record_data))
+        continue
+
+        load(record_data, args.api_url, headers)
+        count += 1
     logger.info('Loading complete')
 
 
