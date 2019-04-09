740fbf5be0d9176befcf90a8d2c2943a13674735
Reed Martz <rmartz@azavea.com>
2019-04-09 12:55:43 -0400

    Restructure operations to reduce values kept in memory

--- app/black_spots/tasks/calculate_black_spots.py
+++ app/black_spots/tasks/calculate_black_spots.py
@@ -49,6 +49,19 @@ def get_latest_segments_tar_uuid(roads_srid, records_csv_obj_id):
     return create_segments_tar(shp_output_dir)
 
 
+def get_forecast_csv_path(segments_shp_uuid, records_csv_obj_id, roads_srid):
+    # - Match events to segments shapefile
+    blackspots_output = get_training_noprecip(
+        segments_shp_uuid,
+        records_csv_obj_id,
+        roads_srid
+    )
+
+    # - Run Rscript to output CSV
+    segments_csv = BlackSpotTrainingCsv.objects.get(pk=blackspots_output).csv.path
+    return forecast_segment_incidents(segments_csv, '/var/www/media/forecasts.csv')
+
+
 @shared_task
 def calculate_black_spots(history_length=datetime.timedelta(days=5 * 365 + 1), roads_srid=3395):
     """Integrates all black spot tasks into a pipeline
@@ -60,37 +73,42 @@ def calculate_black_spots(history_length=datetime.timedelta(days=5 * 365 + 1), r
                                     dynamic number of years without failure.
         roads_srid (int): SRID in which to deal with the Roads data
     """
-    config = BlackSpotConfig.objects.all().order_by('pk').first()
-    if not config:
+    try:
+        severity_percentile_threshold = (
+            BlackSpotConfig.objects.all().order_by('pk').first().severity_percentile_threshold
+        )
+    except AttributeError:
         logger.warn('BlackSpots are not fully configured; set a percentile cutoff first.')
         return
     # Get the parameters we'll use to filter down the records we want
-    now = timezone.now()
-    oldest = now - history_length
     # Note that this assumes that the RecordType with this label to be used will also be marked as
     # `active`. The `load_incidents` script ensures only the most recent record type is set as such.
-    record_type = RecordType.objects.filter(
+    record_type_pk = RecordType.objects.filter(
         label=settings.BLACKSPOT_RECORD_TYPE_LABEL,
         active=True
-    ).first()
+    ).first().pk
 
     # - Get events CSV. This is obtained before the road network segments are calculated
     # as an optimization, so we can ignore roads that won't have any associated records.
-    records_csv_obj_id = export_records(oldest, now, record_type.pk)
+    now = timezone.now()
+    oldest = now - history_length
+    records_csv_obj_id = export_records(
+        oldest,
+        now,
+        record_type_pk
+    )
 
     # Get the UUID, since that is what is used when passing to tasks in the chain
-    segments_shp_uuid = get_latest_segments_tar_uuid(roads_srid, records_csv_obj_id)
+    segments_shp_uuid = get_latest_segments_tar_uuid(
+        roads_srid,
+        records_csv_obj_id
+    )
 
-    # - Match events to segments shapefile
-    blackspots_output = get_training_noprecip(
+    forecasts_csv = get_forecast_csv_path(
         segments_shp_uuid,
         records_csv_obj_id,
         roads_srid
     )
-
-    # - Run Rscript to output CSV
-    segments_csv = BlackSpotTrainingCsv.objects.get(pk=blackspots_output).csv.path
-    forecasts_csv = forecast_segment_incidents(segments_csv, '/var/www/media/forecasts.csv')
     # - Load blackspot geoms from shapefile and CSV
     # The shapefile is stored as a gzipped tarfile so we need to extract it
     tar_output_dir = tempfile.mkdtemp()
@@ -100,12 +118,14 @@ def calculate_black_spots(history_length=datetime.timedelta(days=5 * 365 + 1), r
             # TODO: Extract only the combined segments file, not the entire tarball
             tar.extractall(tar_output_dir)
 
-            logger.info("Performing blackspot calculations")
-            segments_path = os.path.join(tar_output_dir, 'segments', COMBINED_SEGMENTS_SHP_NAME)
-            load_blackspot_geoms(
-                segments_path,
-                forecasts_csv, record_type.pk, roads_srid,
-                output_percentile=config.severity_percentile_threshold
-            )
+        logger.info("Performing blackspot calculations")
+        segments_path = os.path.join(tar_output_dir, 'segments', COMBINED_SEGMENTS_SHP_NAME)
+        load_blackspot_geoms(
+            segments_path,
+            forecasts_csv,
+            record_type_pk,
+            roads_srid,
+            output_percentile=severity_percentile_threshold
+        )
     finally:
         shutil.rmtree(tar_output_dir)
